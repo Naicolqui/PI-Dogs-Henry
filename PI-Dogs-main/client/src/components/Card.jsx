9930b64bfaee8d706c768317ea6f851bf58ae5ab
Nicole Quilmore <nicolequilmore96@gmail.com>
2022-06-25 19:26:08 -0300

    cambios en las cards

--- PI-Dogs-main/client/src/components/Card.jsx
+++ PI-Dogs-main/client/src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Card.css';
 
 export default function Card({name, image, temper, weightMin, weightMax}){
@@ -7,8 +8,11 @@ export default function Card({name, image, temper, weightMin, weightMax}){
             <h2 className="name">{name}</h2>
             <img className="img" src={image} alt="not found" />
             <h4 className="p">{temper}</h4>
-            <h4 className="p">{weightMin}</h4>
-            <h4 className="p">{weightMax}</h4>
+            <div>
+               <h4 className="p">{weightMin}</h4>
+               <h4 className="p">{weightMax}</h4>
+            </div>
+            <Link to='/details'><button>Más información</button></Link>
         </div>
     );
 }
